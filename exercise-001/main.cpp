c4e305c678d74d62abea0cd0a48caefdd9580ba3
Benjamin Wirz <wirz.benjamin-it24@it.dhbw-ravensburg.de>
2025-09-30 15:17:48 +0200

    Excercise 001, Part 1-3

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -3,17 +3,39 @@
 
 #include "config.h"
 
+/* Part 2: Functions with References */
+
+void changeValueByParameter(int value)
+{
+    value = 99;
+    fmt::print("new Value: {}\n", value);
+};
+
+void changeValueByReference(int& ref)
+{
+    ref = 77;
+    fmt::print("new Value: {}\n", ref);
+};
+
 auto main(int argc, char **argv) -> int
 {
+    /* Part 1: Reference Basics */
 
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    fmt::print("Hello, {}!\n", tfe24::PROJECT_NAME);
+    int x = 10;
+    int& r = x;
+    fmt::print("Value of x->r: {}\n", r);
+    r = 42;
+    fmt::print("Value of x->r after change: {}\n", r);
+    fmt::print("\n");
 
-    /* INSERT YOUR CODE HERE */
+    /* Part 3: Comparison */
+
+    int y = 5;
+    fmt::print("Value of y before function: {}\n", y);
+    changeValueByParameter(y);
+    fmt::print("Value of y after changeValueByParameter: {}\n", y);
+    changeValueByReference(y);
+    fmt::print("Value of y after changeValueByReference: {}\n", y);
 
     return 0; /* exit gracefully*/
 }
