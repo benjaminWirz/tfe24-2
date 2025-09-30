3314111e1ffd11b58296594e32234bf018206f04
Benjamin Wirz <wirz.benjamin-it24@it.dhbw-ravensburg.de>
2025-09-30 14:15:26 +0200

    first coding

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -3,17 +3,22 @@
 
 #include "config.h"
 
-auto main(int argc, char **argv) -> int
+auto main(int argc, char **argv) -> int //argc -> argument count, argv -> argument vector
 {
-
+ 
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", tfe24::PROJECT_NAME);
+    fmt::print("Hello World, {}!\n", tfe24::PROJECT_NAME);
 
     /* INSERT YOUR CODE HERE */
+
+    int x = 10;
+    int *p = &x; // & als Adressoperator -> Adresse of x
+    fmt::print("Address of x: {}\nValue of x: {}\n", fmt::ptr(&x), x);
+    //fmt::print("Value of x: {}\n", x);
 
     return 0; /* exit gracefully*/
 }
