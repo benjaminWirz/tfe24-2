b77b6f2a21e08808808893db9770901da2c51390
Benjamin Wirz <wirz.benjamin-it24@it.dhbw-ravensburg.de>
2025-10-21 14:31:52 +0200

    feat: add random number generation and sorting functionality

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -1,5 +1,6 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
+#include <random>
 
 #include "CLI/CLI.hpp"
 #include "config.h"
@@ -28,11 +29,37 @@ auto main(int argc, char** argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
-
     /* INSERT YOUR CODE HERE */
 
     fmt::print("Count: {}\n", counter);
+
+    // Generate random numbers
+    std::random_device r;
+    std::default_random_engine e1(r());
+    std::uniform_int_distribution<int> uniform_dist(1, 100);
+
+    // Store random numbers in a vector
+    fmt::println("-------------------------");
+    fmt::println("random numbers:");
+
+    std::vector<int> random_vec;
+    for (int i = 1; i <= counter; ++i) {
+        random_vec.push_back(uniform_dist(e1));
+        fmt::print("{}\n", random_vec.back());
+    }
+
+    //sort and print the numbers
+    fmt::println("-------------------------");
+    fmt::println("sorted numbers:");
+
+    std::vector<int> sorted_vec = random_vec;
+    std::sort(sorted_vec.begin(), sorted_vec.end());
+
+    for (const auto& num : sorted_vec) {
+        fmt::print("{}\n", num);
+    }
+
+    fmt::println("-------------------------");
 
     
 
