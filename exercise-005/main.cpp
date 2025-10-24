c99a518d06443739ca4ca6e7c7f1c7eb94b1834e
Benjamin Wirz <wirz.benjamin-it24@it.dhbw-ravensburg.de>
2025-10-24 14:19:56 +0200

    feat: add Point class with move and print methods

--- exercise-005/main.cpp
+++ exercise-005/main.cpp
@@ -35,5 +35,14 @@ auto main(int argc, char **argv) -> int
 
     /* INSERT YOUR CODE HERE */
 
+    Point p1(2, 3);     // Erzeugen eines Punktes (2, 3)
+    p1.print();         // Ausgabe: (2, 3)
+
+    p1.move(1, -1);     // Verschiebe um (1, -1)
+    p1.print();         // Ausgabe: (3, 2)
+
+    Point p2;           // Standardkonstruktor -> (0, 0)
+    p2.print();         // Ausgabe: (0, 0)
+
     return 0; /* exit gracefully*/
 }
